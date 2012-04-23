5321323ea22647b2d464595dddfab4ac92830718
Fernando Miguel Carvalho <mcarvalho@cc.isel.ipl.pt>
2012-04-23 14:13:20 +0100

    minor fix

--- aula18-interpreter-factory-decorator/src/main/java/cmdsh/core/CmdPrintTime.java
+++ aula18-interpreter-factory-decorator/src/main/java/cmdsh/core/CmdPrintTime.java
@@ -1,10 +1,15 @@
 package cmdsh.core;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import cmdsh.parsers.ArgsParserOrdered;
 
 public class CmdPrintTime extends AbstractCommand{
 	private final AbstractCommand cmd;
-
+	private static final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	private static final Calendar cal = Calendar.getInstance();
 	public CmdPrintTime(AbstractCommand cmd) {
 		super(cmd.name, new ArgsParserOrdered(), new IArgument[]{});
 		this.cmd = cmd;
@@ -12,7 +17,7 @@ public class CmdPrintTime extends AbstractCommand{
 	@Override
 	public void performCommand(String params) {
 		cmd.performCommand(params);
-		System.out.println("time: " + System.currentTimeMillis());
+		System.out.println("time: " + dateFormat.format(cal.getTime()));
 	}
 	@Override
 	protected void executeCommand() {		
